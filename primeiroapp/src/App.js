e72e24de53dacd69c028706c8eccab28911edc78
kisscompany <111711991+kisscompany@users.noreply.github.com>
2025-01-08 19:22:50 -0300

    Entendendo props e components

--- primeiroapp/src/App.js
+++ primeiroapp/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const Bemvindo = (props) => {
+  return (
+    <div>
+      <h2>Bem vindo(a), {props.nome}</h2>
+      <h3>Sua idade é: {props.idade} Anos</h3>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Olá amigo!
+      <Bemvindo nome='Wendel' idade='28' />
+      <h1>Tudo nos conformes meu chapa?</h1>
     </div>
-  );
+  )
 }
 
 export default App;
