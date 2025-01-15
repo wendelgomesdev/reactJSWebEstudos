24d570df35548108dfefc758a749c89863b1198f
kisscompany <111711991+kisscompany@users.noreply.github.com>
2025-01-15 18:35:48 -0300

    renderização condicional

--- primeiroapp/src/App.js
+++ primeiroapp/src/App.js
@@ -1,13 +1,15 @@
 import EstudosComponents from './Components';
 import ComponentComState from './States';
 import CicloDeVida from './CicloDeVida';
+import RenderizacaoCondicional from './RenderizacaoCondicional'
 
 function App() {
   return (
     <div>
       {/* <EstudosComponents /> */}
       {/* <ComponentComState /> */}
-      <CicloDeVida />
+      {/* <CicloDeVida /> */}
+      <RenderizacaoCondicional />
     </div>
   )
 }
---
+++ primeiroapp/src/RenderizacaoCondicional.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class RenderizacaoCondicional extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            status: false
+        };
+
+        this.entrar = this.entrar.bind(this);
+        this.sair = this.sair.bind(this);
+    }
+
+    entrar(){
+        this.setState({status: true});
+    }
+
+    sair(){
+        this.setState({status: false});
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.status ?
+                    <div>
+                        <h2>Bem-vindo ao sistema</h2>
+                        <button onClick={this.sair}>Sair</button>
+                    </div> :
+                    <div>
+                        <h2>Olá, visitante, faça login!</h2>
+                        <button onClick={this.entrar}>Entrar</button>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+export default RenderizacaoCondicional
